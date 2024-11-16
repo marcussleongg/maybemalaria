a85b2f703326174eb15ec64828b4a61c43db1c54
Yonathan Abera <yabera@Yonathans-MacBook-Air-672.local>
2024-11-16 18:27:09 -0500

    Added Location Search Functionality

---
+++ script2.js
@@ -0,0 +1,23 @@
+let autocomplete;
+
+document.addEventListener("DOMContentLoaded", () => {
+  initAutocomplete();
+});
+
+function initAutocomplete() {
+  const input = document.getElementById("autocomplete");
+  autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.addListener("place_changed", handlePlaceSelect);
+}
+
+function handlePlaceSelect() {
+  const place = autocomplete.getPlace();
+  if (!place.geometry) {
+    return;
+  }
+  const lat = place.geometry.location.lat();
+  const lng = place.geometry.location.lng();
+  console.log(lat, lng);
+  return [lat, lng];
+  
+}
