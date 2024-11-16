7b0dd640a4be1b1287c2e1bcd6e4ebd8b18d1de4
Marcus Leong <162563608+marcussleongg@users.noreply.github.com>
2024-11-16 16:52:43 -0500

    Add functionality for weather API

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const body = document.querySelector('body');
-const weatherUrl = "http://api.weatherapi.com/v1/current.json?key=6c99696223224d338cc210510241611&q=London&aqi=no";
+const weatherUrl = "http://api.weatherapi.com/v1/current.json?key=6c99696223224d338cc210510241611&q=9.005401,38.763611&aqi=no";
 function httpGet(theUrl) {
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.open( "GET", theUrl, false );
         xmlHttp.send( null );
         return xmlHttp.responseText;
-    }
-body.appendChild(httpGet(weatherUrl));
+}
+const jsonStr = httpGet(weatherUrl);
+const jsonObj = JSON.parse(jsonStr);
+console.log(httpGet(weatherUrl));
+const tempF = jsonObj.current.temp_f;
+const tempC = jsonObj.current.temp_c;
+const precipitationMm = jsonObj.current.precip_mm;
+const precipitationIn = jsonObj.current.precip_in;
+const humidity = jsonObj.current.humidity;
+console.log('hi');
+console.log(`temperature in farenheit is ${tempF}`);
+console.log(`temperature in celcius is ${tempC}`);
+console.log(`precipitation in mm is ${precipitationMm}`);
+console.log(`precipitation in inches is ${precipitationIn}`);
+console.log(`humidity percentage is ${humidity}`);
