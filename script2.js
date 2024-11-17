4ba080e48b97efad79ac9ddab29f2048b36dc3c6
Yonathan Abera <yabera@Yonathans-MacBook-Air-672.local>
2024-11-16 23:01:40 -0500

    Trail for elevation intergration

--- script2.js
+++ script2.js
@@ -15,8 +15,27 @@ function handlePlaceSelect() {
   if (!place.geometry) {
     return;
   }
+
   const lat = place.geometry.location.lat();
   const lng = place.geometry.location.lng();
   console.log(lat, lng);
+
+
+  const elevationUrl = `https://maps.googleapis.com/maps/api/elevation/json?locations=${lat}%2C${lng}&key=AIzaSyAU-qMK7iqlaPfllH4w5BCqW2GI6X_sf1M`;
+
+
+  fetch(elevationUrl)
+    .then(response => response.json()) 
+    .then(data => {
+      if (data.status === "OK") {
+        const elevation = data.results[0].elevation;
+        console.log(`Elevation: ${elevation} meters`);
+      } else {
+        console.error(`Elevation API error: ${data.status}`);
+      }
+    })
+    .catch(error => console.error("Error fetching elevation data:", error));
+
   return [lat, lng];
 }
+
