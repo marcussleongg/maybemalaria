8d9a0119372419ccca4ba02fa6e39d4b147b8e0e
Yonathan Abera <yabera@Yonathans-MacBook-Air-672.local>
2024-11-16 23:45:38 -0500

    Fixed altitude problems

--- script2.js
+++ script2.js
@@ -21,20 +21,13 @@ function handlePlaceSelect() {
   console.log(lat, lng);
 
 
-  const elevationUrl = `https://maps.googleapis.com/maps/api/elevation/json?locations=${lat}%2C${lng}&key=AIzaSyAU-qMK7iqlaPfllH4w5BCqW2GI6X_sf1M`;
-
-
-  fetch(elevationUrl)
-    .then(response => response.json()) 
-    .then(data => {
-      if (data.status === "OK") {
-        const elevation = data.results[0].elevation;
-        console.log(`Elevation: ${elevation} meters`);
-      } else {
-        console.error(`Elevation API error: ${data.status}`);
-      }
-    })
-    .catch(error => console.error("Error fetching elevation data:", error));
+  const elevationUrl = `https://api.open-meteo.com/v1/elevation?latitude=${lat}&longitude=${lng}`;
+    fetch(elevationUrl)
+        .then((response) => response.json())
+        .then((data) => {
+        const elevation = data.elevation[0];
+        console.log(elevation);
+        });
 
   return [lat, lng];
 }
