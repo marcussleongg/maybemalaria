76daab80390db3458a02bc482f56cfe142c5d411
Marcus Leong <162563608+marcussleongg@users.noreply.github.com>
2024-11-17 10:27:31 -0500

    Amend DOM from genAI script

--- genai.js
+++ genai.js
@@ -9,12 +9,38 @@ const tempText = document.querySelector('#tempText').textContent;
 const precipText = document.querySelector('#precipText').textContent;
 const humidityText = document.querySelector('#humidityText').textContent;
 const altitudeText = document.querySelector('#altitudeText').textContent;
+var infantText = document.querySelector('#infantText');
+if (infantText != null) {
+    infantText = document.querySelector('#infantText').textContent;
+}
+var hivaidsText = document.querySelector('#hivaidsText');
+if (hivaidsText != null) {
+    hivaidsText = document.querySelector('#hivaidsText').textContent;
+}
+var pregnantText = document.querySelector('#pregnantText');
+if (pregnantText != null) {
+    pregnantText = document.querySelector('#pregnantText').textContent;
+}
+var travelingText = document.querySelector('#travelingText');
+if (travelingText != null) {
+    travelingText = document.querySelector('#travelingText').textContent;
+}
+var ageText = document.querySelector('#ageText');
+if (ageText != null) {
+    ageText = document.querySelector('#ageText').textContent;
+}
 
 // Your AI prompt
-const prompt = `What are preventive measures for malaria for at an area where ${tempText}, ${precipText}, ${humidityText}, ${altitudeText}`;
+const prompt = `Without the precautionary text telling me to check with health advisories, what are preventive measures for malaria for at an area where ${tempText}, ${precipText}, ${humidityText}, ${altitudeText}, and if ${infantText}, ${hivaidsText}, ${pregnantText}, ${travelingText}, ${ageText}`;
 async function getContent() {
     const result = await model.generateContent(prompt);
-    console.log(result.response.text());
+    const container = document.querySelector('.container');
+    const suggestion = document.createElement('section');
+    const suggestionText = document.createElement('p');
+    suggestionText.textContent = result.response.text();
+    container.appendChild(suggestion);
+    suggestion.appendChild(suggestionText);
+    //console.log(result.response.text());
 }
 
 getContent();
